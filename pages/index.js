8c89f93971bb38441b52e979e9296ec8f6cdcd88
Daniel Barros <94022875+Barros263inf@users.noreply.github.com>
2026-07-30 04:47:54 +0000

    Add pages directory

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <>
+            <h1>Tell me, what is it you plan to dowith your one wild and precious life?</h1>
+        </>
+    )
+}
+
+export default Home;
