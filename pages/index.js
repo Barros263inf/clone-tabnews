79e33f378ca1cc4d05345dcbd5a90d7670f19d41
Daniel Barros <94022875+Barros263inf@users.noreply.github.com>
2026-07-31 16:32:56 +0000

    Add formatter scripts

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,35 @@
 function Home() {
-    return(
-        <>
-            <div style={{
-                width: "100%",
-                height: "98vh",
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-                justifyContent: "center",
-                }}>
-                <h1>Tell me, what is it you plan to dowith your one wild and precious life?</h1>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "98vh",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <h1>
+          Tell me, what is it you plan to dowith your one wild and precious
+          life?
+        </h1>
+      </div>
+    </>
+  );
+}
+
+function test() {
+  console.log("test");
+}
+
+function teste() {
+  console.log("ola");
+}
+
+function teste1() {
+  console.log();
 }
 
 export default Home;
