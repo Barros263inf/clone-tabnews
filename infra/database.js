f41aed6141615a250a337170deff51ad77bdd41b
Barros263inf <daniel.dev.silva@gmail.com>
2026-08-26 09:38:26 -0300

    Refact to use ssl only in production

--- infra/database.js
+++ infra/database.js
@@ -32,7 +32,7 @@ function getSSLValue() {
   if (process.env.POSTGRES_CA) {
     return process.env.POSTGRES_CA;
   }
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
 export default {
