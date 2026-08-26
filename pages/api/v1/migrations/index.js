51dc775230babc0fd1fe7012fb5d453980fb1d3e
Barros263inf <daniel.dev.silva@gmail.com>
2026-08-26 09:45:16 -0300

    Change response status code to 201 when any migrations was runned in went run

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -22,7 +22,9 @@ async function migrations(request, response) {
       ...migrationOptions,
       dryRun: false,
     });
-
+    if (migratedMigrations.length > 0) {
+      response.status(201).json(migratedMigrations);
+    }
     response.status(200).json(migratedMigrations);
   }
 
