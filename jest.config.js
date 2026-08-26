2ba6af0dbab88bc4cf066fcb18de3a4164a1abb5
Barros263inf <daniel.dev.silva@gmail.com>
2026-08-26 09:41:22 -0300

    Able Jest to use ESM modules and load .env files

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,14 @@
-import nextJest from "next/jest";
+import dotenv from "dotenv";
+import nextJest from "next/jest.js";
+
+dotenv.config({
+  path: ".env.development",
+});
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+export default jestConfig;
